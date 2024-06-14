a0b2776b562cba4528b78b78430cc632b2d4dad9
madyanes <madyanes.dev@gmail.com>
2024-06-14 20:56:05 +0700

    [FEAT] debouncing trick

---
+++ DeveloperTricks/debouncing.js
@@ -0,0 +1,39 @@
+/**
+ * What is Debouncing?
+ *
+ * Debouncing is a technique to limit the rate at which a function is executed.
+ * When multiple events are triggered in quick succession,
+ * the debounce function will ensure that only the last event in the series triggers the function execution after a specified delay.
+ */
+
+/**
+ * Why Use Debouncing?
+ *
+ * => Performance Optimization: Prevents performance issues by reducing the number of times a function is called.
+ * => Enhanced User Experience: Avoids the clutter of repeated actions, providing a smoother experience.
+ * => Network Efficiency: Reduces unnecessary network requests when used with event handlers like input fields for live search.
+ */
+
+/**
+ * How Debouncing Works
+ *
+ * Imagine a user typing in a search box that triggers an API call for each keystroke.
+ * Without debouncing, each keystroke would result in a new API call, flooding the network with requests.
+ * With debouncing, only the final input after the user stops typing for a specified duration will trigger the API call.
+ */
+
+function debounce(func, wait) {
+  let timeout
+
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout)
+      func(...args)
+    }
+
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
+
+// Reference: https://dev.to/dipakahirav/understanding-debouncing-in-javascript-5g30?ref=dailydev
